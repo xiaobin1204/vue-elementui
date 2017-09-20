49c4e981cf8f58d2f046d8f2e67557f676630158
wangbin <wblucky@126.com>
2017-09-20 11:52:00 +0000

    table组件

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,4 +1,41 @@
 <template lang="html">
+<div>
+    <el-table
+      ref="DataTable"
+      :data="dataStore.datas"
+      border
+      tooltip-effect="dark"
+      style="width: 100%"
+      :default-sort = "{prop: 'date', order: 'descending'}"
+      @selection-change="handleSelectionChange">
+    <el-table-column
+        prop="name"
+        label="姓名"
+        show-overflow-tooltip>
+      </el-table-column>
+    <el-table-column
+        prop="age"
+        label="年龄"
+        v-if="dataStore.isShow"
+        sortable
+        show-overflow-tooltip>
+      </el-table-column>
+    <el-table-column
+        fixed="right"
+        v-if="softlistStore.isShow"
+        label="操作"
+        width="140">
+        <template scope="scope">
+          <el-button
+            @click.native.prevent="check(scope.$index)"
+            type="primary"
+            size="mini">
+            点一下试一试
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+</div>
 </template>
 <script>
 </script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,19 @@
 <template lang="html">
 <div>
   <h1>
-    this is home.
+    欢迎 {{ this.userStore.authUser.name }}   
   </h1>
 </div>
 </template>
 <script>
+import {mapState} from 'vuex'
+export default {
+  computed: {
+    ...mapState({
+      userStore: state => state.userStore
+    })
+  }
+}
 </script>
 <style lang="css" scoped>
 </style>
